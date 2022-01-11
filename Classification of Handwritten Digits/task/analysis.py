9dc4a3773356c84be0e24932a42c614ef699ce5b
elena <ikonnikovaev@gmail.com>
2022-01-11 14:03:18 +0300

    Stage 3

--- Classification of Handwritten Digits/task/analysis.py
+++ Classification of Handwritten Digits/task/analysis.py
@@ -3,6 +3,18 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+def fit_predict_eval(model, features_train, features_test, target_train, target_test):
+    model.fit(features_train, target_train)
+    target_pred = model.predict(features_test)
+    score = accuracy_score(target_test, target_pred)
+    print(f'Model: {model}\nAccuracy: {score}\n')
+    return score
 
 (x_train, y_train), (x_test, y_test) = \
     tf.keras.datasets.mnist.load_data(path="mnist.npz")
@@ -19,14 +31,31 @@ features = x_train[:6000]
 targets = y_train[:6000]
 my_x_train, my_x_test, my_y_train, my_y_test =\
     train_test_split(features, targets, test_size=0.3, random_state=40)
-print(f"x_train shape: {my_x_train.shape}")
-print(f"x_test shape: {my_x_test.shape}")
-print(f"y_train shape: {my_y_train.shape}")
-print(f"y_test shape: {my_y_test.shape}")
 
-vc = pd.Series(my_y_train).value_counts(normalize=True)
-print("Proportion of samples per class in train set:")
-for c in np.unique(y_train):
-    print(f"{c} {vc[c]}")
+models = [KNeighborsClassifier(),
+          DecisionTreeClassifier(random_state=40),
+          LogisticRegression(random_state=40),
+          RandomForestClassifier(random_state=40)
+]
+best_model = None
+best_score = 0
+for m in models:
+    score = fit_predict_eval(
+        model=m,
+        features_train=my_x_train,
+        features_test=my_x_test,
+        target_train=my_y_train,
+        target_test=my_y_test
+    )
+    if score > best_score:
+        best_model = m
+        best_score = score
+n = str(best_model).find('(')
+str_best_model = str(best_model)[:n]
+print(f'The answer to the question: {str_best_model} - {round(best_score, 3)}')
+
+
+
+
 
 
