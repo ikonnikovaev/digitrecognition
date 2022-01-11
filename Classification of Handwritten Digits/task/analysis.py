7f64667af6a29ce9a23fb038b52f713e4187f746
elena <ikonnikovaev@gmail.com>
2022-01-11 20:02:18 +0300

    Stage 4

--- Classification of Handwritten Digits/task/analysis.py
+++ Classification of Handwritten Digits/task/analysis.py
@@ -1,9 +1,9 @@
 # write your code here
-import numpy as np
-import pandas as pd
+
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import Normalizer
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -15,7 +15,7 @@ def fit_predict_eval(model, features_train, features_test, target_train, target_
     score = accuracy_score(target_test, target_pred)
     print(f'Model: {model}\nAccuracy: {score}\n')
     return score
-
+''
 (x_train, y_train), (x_test, y_test) = \
     tf.keras.datasets.mnist.load_data(path="mnist.npz")
 (n, k, l) = x_train.shape
@@ -37,6 +37,7 @@ models = [KNeighborsClassifier(),
           LogisticRegression(random_state=40),
           RandomForestClassifier(random_state=40)
 ]
+'''
 best_model = None
 best_score = 0
 for m in models:
@@ -50,12 +51,32 @@ for m in models:
     if score > best_score:
         best_model = m
         best_score = score
+
 n = str(best_model).find('(')
 str_best_model = str(best_model)[:n]
-print(f'The answer to the question: {str_best_model} - {round(best_score, 3)}')
+print(f'Without normalization: {str_best_model} - {round(best_score, 3)}\n')
+'''
 
-
-
-
-
+normalizer = Normalizer()
+x_train_norm = normalizer.fit_transform(my_x_train)
+x_test_norm = normalizer.transform(my_x_test)
+models_scores = {}
+for m in models:
+    v = fit_predict_eval(
+        model=m,
+        features_train=x_train_norm,
+        features_test=x_test_norm,
+        target_train=my_y_train,
+        target_test=my_y_test
+    )
+    n = str(m).find('(')
+    k = str(m)[:n]
+    models_scores[k] = v
+print(f'The answer to the 1st question: yes\n')
+sorted_scores = sorted(models_scores.items(), key=lambda x: x[1], reverse=True)
+answer2 = f'The answer to the 2nd question: '
+for (k, v) in sorted_scores[:2]:
+    answer2 += f'{k}-{round(v, 3)}, '
+answer2 = answer2[:-2]
+print(answer2)
 
